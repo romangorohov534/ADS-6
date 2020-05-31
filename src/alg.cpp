328172b45b4cba68b4c8279b6e5c11c61703491a
romangorohov534 <62945887+romangorohov534@users.noreply.github.com>
2020-05-31 22:29:45 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,42 +1,43 @@
-#include  <fstream>
-#include  <locale>
-#include  <cstdlib>
+
+#include <fstream>
+#include <locale>
+#include <cstdlib>
 using namespace std;
-#include <cctype> 
+#include <cctype>
 
 BST<std::string> makeTree(char* filename)
 {
 
-} 
-	BST<std::string>BS;
-	ifstream f;
-	f.open(filename);
-	string s = "", temp = "";
-	int i = 0;
-	bool flag = false;
-	while (f >> s)
-	{
-		for (int j = 0; j < s.size(); j++)
-		{
-			flag = false;
-			if ((s[j] >= 'a' && s[j] <= 'z') || (s[j] >= 'A' && s[j] <= 'Z'))
-				temp += (char)tolower(s[j]);
-			else
-			{
-				if (temp != "")
-				{
-					BS.add(temp);
-					temp = "";
-					flag = true;
-					continue;
-				}
-			}
-		}
-		if (flag == false && temp != "")
-		{
-			BS.add(temp);
-			temp = "";
-		}
-	}
-	return BS;
+}
+BST<std::string>BS;
+ifstream f;
+f.open(filename);
+string s = "", temp = "";
+int i = 0;
+bool flag = false;
+while (f » s)
+{
+for (int j = 0; j < s.size(); j++)
+{
+flag = false;
+if ((s[j] >= 'a' && s[j] <= 'z') || (s[j] >= 'A' && s[j] <= 'Z'))
+temp += (char)tolower(s[j]);
+else
+{
+if (temp != "")
+{
+BS.add(temp);
+temp = "";
+flag = true;
+continue;
+}
+}
+}
+if (flag == false && temp != "")
+{
+BS.add(temp);
+temp = "";
+}
+}
+return BS;
 }
