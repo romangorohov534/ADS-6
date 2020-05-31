d5722dee4d5fa94715c46496a7ea7e3a3543a4fa
romangorohov534 <62945887+romangorohov534@users.noreply.github.com>
2020-05-31 22:39:47 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,39 +1,88 @@
-#include <cassert>
-#include <iostream>
-using namespace std;
-template<typename T>
+#include  <fstream>
+#include  <locale>
+#include  <cstdlib>
+#include  <cctype>
+
 BST<std::string> makeTree(char* filename)
 {
+    char sim, * text, * value;
+    int count, num, begin = 0, end = 0;
+    std::string str;
 
-}
-BST<std::string>BS;
-ifstream f;
-f.open(filename);
-string s = "", temp = "";
-int i = 0;
-bool flag = false;
-while (f » s)
-{
-for (int j = 0; j < s.size(); j++)
-{
-flag = false;
-if ((s[j] >= 'a' && s[j] <= 'z') || (s[j] >= 'A' && s[j] <= 'Z'))
-temp += (char)tolower(s[j]);
-else
-{
-if (temp != "")
-{
-BS.add(temp);
-temp = "";
-flag = true;
-continue;
-}
-}
-}
-if (flag == false && temp != "")
-{
-BS.add(temp);
-temp = "";
-}
-return BS;
+    BST<std::string> tree;
+
+    std::ifstream fin(filename);
+
+    if (!fin.is_open()) {
+		std::cout << "File not found. Try again." << std::endl;
+		return tree;
+	}
+
+	while (fin.get(sim)) {
+			count++;
+	}
+	fin.close();
+
+    if (count == 0) {
+		std::cout << "You did not enter anything." << std::endl;
+		return tree;
+	}
+
+	count++;
+	text = new char[count];
+
+	fin.open(filename);
+	for (int i = 0; i < count-1; i++) {
+		fin.get(sim);
+		text[i] = sim;
+	}
+	fin.close();
+
+	text[count-1] = '\0';
+
+	while (begin < count) {
+		if (isalpha((unsigned char)text[begin])) {
+			end = begin;
+			if (end + 1 < count) {
+				while (isalpha((unsigned char)text[end+1])) {
+					end++;
+					if (end + 1 > count) break;
+				}
+			}
+
+			if (end + 1 < count) {
+				if (!isspace((unsigned char)text[end+1]) && !ispunct((unsigned char)text[end+1]) 
+				&& (unsigned char)text[end+1] != '\0') {
+					begin = end + 1;
+					while (!isspace((unsigned char)text[begin]) && !ispunct((unsigned char)text[begin]) 
+					&& (unsigned char)text[begin] !='\0') {
+						begin++;
+						if (begin > count) break;
+					}
+					continue;
+				}
+			}
+
+			num = end - begin + 2;
+			value = new char[num];
+			for (int i = 0; i < num - 1; i++) {
+			    if (isupper((unsigned char)text[begin])) {
+			        text[begin] = tolower(text[begin]);
+			    }
+			    value[i] = text[begin];
+			    begin++;
+			}
+			value[num - 1] = '\0';
+			std::string str = value;
+
+			tree.add(str);
+			delete[] value;
+		}
+		else begin++;
+	}
+
+
+    delete[] text;
+
+    return tree;
 }
